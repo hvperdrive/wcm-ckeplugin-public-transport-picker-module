16ba2bcf6839f4fccaf7586818dcfd947c24665b
Lars Van Poucke <larsundefined@gmail.com>
2019-11-27 16:38:38 +0100

    version 1.0.1

--- public/app/services/ckeditorModulePlugins.service.js
+++ public/app/services/ckeditorModulePlugins.service.js
@@ -1,7 +1,7 @@
 "use-strict";
 
 (function() {
-	angular.module("ckeplugin-public-transport-picker_1.0.0")
+	angular.module("ckeplugin-public-transport-picker_1.0.1")
 		.service("ckeditorPluginDefinitionsPublicTransportPicker", [
 
 			"CKEditorConfigPublicTransportPicker",
